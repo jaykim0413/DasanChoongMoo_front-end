49c14ecdc6b5fd704c915d138160e3fc1743e21f
jaykim0413 <jaylego101@gmail.com>
2023-03-06 03:32:52 +0900

    Basic Framework(in development)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+// COMPONENTS
+
+function Navbar() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+function Footer() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+// PAGES
+function Login() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+function Register() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+function Schedule() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+function ClassInfo() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+function ClassAnnouncement(){
+  return (
+    <>
+    
+    </>
+  )
+}
+
+function PersonalAnnouncement() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+function Settings() {
+  return (
+    <>
+    
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    </>
   );
 }
 
